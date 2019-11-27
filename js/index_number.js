9e3878dad907d06cb296729303b9d20b506df2dd
Jorge Fabregas <jorge.fabregas22@gmail.com>
2019-11-26 22:37:24 -0500

    number

--- js/index_number.js
+++ js/index_number.js
@@ -2,10 +2,9 @@ let butnumber = document.getElementById ("numbut");
 
 butnumber.addEventListener ("click", function (){
 
-    let num = document.getElementById ("num".value);
+    let num = document.getElementById ("num").value
     
     if (num > 0) {
-        console.log : Your number is positive;
-        alert : "Your number is positive"
+        alert (`Your number is positive`)
     }
 })
