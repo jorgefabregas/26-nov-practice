acb40a39cf593cb0b335bfb5496be6fb73f011e5
Jorge Fabregas <jorge.fabregas22@gmail.com>
2019-11-26 22:15:56 -0500

    Question 2 starts

---
+++ js/index_number.js
@@ -0,0 +1,11 @@
+let butnumber = document.getElementById ("numbut");
+
+butnumber.addEventListener ("click", function (){
+
+    let num = document.getElementById ("num".value);
+    
+    if (num > 0) {
+        console.log : Your number is positive;
+        alert : "Your number is positive"
+    }
+})
